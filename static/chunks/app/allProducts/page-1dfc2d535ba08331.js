c559e9ac5af4c1816e82e6ea1963f122e92908d4
nasir-tafsol <nasir.js@tafsol.com>
2024-11-02 03:26:14 +0500

    added files

---
+++ static/chunks/app/allProducts/page-1dfc2d535ba08331.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[93],{6669:function(n,e,u){Promise.resolve().then(u.bind(u,932))}},function(n){n.O(0,[30,145,892,932,971,117,744],function(){return n(n.s=6669)}),_N_E=n.O()}]);
---
+++ static/chunks/app/page-c680379d1eeab69d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{102:function(e,n,s){Promise.resolve().then(s.bind(s,932)),Promise.resolve().then(s.bind(s,99))}},function(e){e.O(0,[30,145,892,932,971,117,744],function(){return e(e.s=102)}),_N_E=e.O()}]);
